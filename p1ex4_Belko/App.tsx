560d37b7608dfc29fd501c32be6df42f2aa4d068
1 <94632891+Vovka0777@users.noreply.github.com>
2025-09-12 15:20:09 +0300

    Update App.tsx

--- p1ex4_Belko/App.tsx
+++ p1ex4_Belko/App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Button, ColorValue, Text, View} from 'react-native';
 
-export default function App() {
+type CatProps = {
+  name: string;
+};
+
+const Cat = (props: CatProps) => {
+  const [isHungry, setIsHungry] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        Я {props.name}, и я {isHungry ? 'голоднен' : 'сыт'}!
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? 'Покорми меня, пожалуйста!' : 'Спасибо!'}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Cafe = () => {
+  return (
+    <>
+      <p><Cat name="Вася"/>
+      <Cat name="Рыжик" />
+      <Cat name="Барсик" />
+      </p>
+    </>
+  );
+};
+
+export default Cafe;
