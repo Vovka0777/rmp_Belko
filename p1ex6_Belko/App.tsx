7ad953d824272a8b506359a671915abe1ec3c543
1 <94632891+Vovka0777@users.noreply.github.com>
2025-09-17 15:42:47 +0300

    flag

---
+++ p1ex6_Belko/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 3, backgroundColor: 'red'}} />
+      <View style={{flex: 2, backgroundColor: 'green'}} />
+      <View style={{flex: 3, backgroundColor: 'red'}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    aspectRatio: 3,
+    padding: 20,
+  },
+});
+
+export default Flex;
